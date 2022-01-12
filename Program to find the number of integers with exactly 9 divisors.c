3af4c1e146966782caacb755ff0da8399e8b185d
Ravikant Sharma <97437176+Ravikant1043@users.noreply.github.com>
2022-01-12 15:13:15 +0530

    #include<stdio.h> int main() {     int a,i,j,t;     int count=0;     scanf("%d",&a);     for(i=1;i<=a;i++)     {count=0;         for(j=1;j<=i;j++)         {             if(i%j==0)             {                 count++;             }         }         if(count==9)        { printf("%d ",i);            t++;        }
        }
        printf("
    Total=%d",t);
        return 0;
    }

---
+++ Program to find the number of integers with exactly 9 divisors.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int a,i,j,t;
+    int count=0;
+    scanf("%d",&a);
+    for(i=1;i<=a;i++)
+    {count=0;
+        for(j=1;j<=i;j++)
+        {
+            if(i%j==0)
+            {
+                count++;
+            }
+        }
+        if(count==9)
+       { printf("%d ",i);
+           t++;
+       }
+       
+    }
+    printf("
+Total=%d",t);
+    return 0;
+}
