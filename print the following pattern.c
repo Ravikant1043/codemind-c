09ee8a58e39b460623a4ff8414ae6970c9330e0c
Ravikant Sharma <97437176+Ravikant1043@users.noreply.github.com>
2022-01-14 15:16:30 +0530

    #include<stdio.h> int main() {     int a,n,i,j=1,count=1;     scanf("%d",&a);     for(i=a;i>0;i--)     {         while(j<=a)         {             printf("%c ",(char)(i+64));             j++;         }count++;         j=count;         printf(" ");     } }

--- print the following pattern.c
+++ print the following pattern.c
@@ -1,21 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int a,i,j,k;
+    int a,n,i,j=1,count=1;
     scanf("%d",&a);
-    for (  i = a; i >=1; i--)
+    for(i=a;i>0;i--)
     {
-        for (j = 1; j < i; j++)
+        while(j<=a)
         {
-            printf(" ");
-        }
-        for (k = 1; k <= a; k++)
-        {
-            if(k==1||k==a||i==1||i==a)
-            printf("*");
-            else
-            printf(" ");
-        }       
+            printf("%c ",(char)(i+64));
+            j++;
+        }count++;
+        j=count;
         printf("
 ");
     }
