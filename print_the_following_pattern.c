2d1037ac9186bd2a8a7283f7cca9ca33b9469623
Ravikant Sharma <97437176+Ravikant1043@users.noreply.github.com>
2022-01-26 09:38:49 +0530

    #include<stdio.h> int main() {     int i,j,n;     scanf("%d",&n);     for(i=n;i>0;i--)     {         for(j=1;j<=i;j++)         {             printf("%d",j);         }         printf(" ");     }     return 0; }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -2,16 +2,15 @@
 int main()
 {
     int i,j,n;
-    //printf("enter a number:");
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    for(i=n;i>0;i--)
     {
-    for(j=1;j<=n;j++)
-    {
-        printf("%c ",(i+64));
-    }
+        for(j=1;j<=i;j++)
+        {
+            printf("%d",j);
+        }
         printf("
 ");
- }
-return 0;
+    }
+    return 0;
 }
