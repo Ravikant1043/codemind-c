d4ca2cbe8b465a3cba6947148519d1156a896fff
Ravikant Sharma <97437176+Ravikant1043@users.noreply.github.com>
2022-05-04 11:43:41 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     int a[n],o=0,e=0;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);         if(a[i]%2==0)         e++;         else         o++;     }     int ev[e],od[o];     e=0,o=0;     for(i=0;i<n;i++)     {         // scanf("%d",&a[i]);         if(a[i]%2==0)         {             ev[e++]=a[i];         }         else         od[o++]=a[i];     }     i=0;     while(i<e)     {         a[i]=ev[i];         i++;     }int k=0;     while(i<e+o)     {         a[i]=od[k++];         i++;     }     for(i=0;i<n;i++)     printf("%d ",a[i]); }

---
+++ even_elements_followed_by_odd.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    int a[n],o=0,e=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+        if(a[i]%2==0)
+        e++;
+        else
+        o++;
+    }
+    int ev[e],od[o];
+    e=0,o=0;
+    for(i=0;i<n;i++)
+    {
+        // scanf("%d",&a[i]);
+        if(a[i]%2==0)
+        {
+            ev[e++]=a[i];
+        }
+        else
+        od[o++]=a[i];
+    }
+    i=0;
+    while(i<e)
+    {
+        a[i]=ev[i];
+        i++;
+    }int k=0;
+    while(i<e+o)
+    {
+        a[i]=od[k++];
+        i++;
+    }
+    for(i=0;i<n;i++)
+    printf("%d ",a[i]);
+}
