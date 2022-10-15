43187e1be1c4e3ee83221b628015c232ed438cc1
Ravikant Sharma <97437176+Ravikant1043@users.noreply.github.com>
2022-10-15 23:27:12 +0530

    #include<stdio.h> int main() {     int i,n,arr[100],k,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d",&k);     for(i=0;i<n;i++)     {         if(arr[i]==k)         {             c=1;         }     }     if(c==1)     {         printf("True");     }     else     {         printf("False");     }     return 0; }

--- element_search_in_an_array.c
+++ element_search_in_an_array.c
@@ -1,20 +1,27 @@
 #include<stdio.h>
 int main()
 {
-    int n;
+    int i,n,arr[100],k,c=0;
     scanf("%d",&n);
-    int a[n],i,b;
-    for (i=0;i<n;i++)
-    scanf("%d",&a[i]);
-    scanf("%d",&b);
     for(i=0;i<n;i++)
     {
-        if(a[i]==b)
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d",&k);
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]==k)
         {
-            printf("True");
-            return 0;
+            c=1;
         }
     }
-    printf("False");
+    if(c==1)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
     return 0;
 }
