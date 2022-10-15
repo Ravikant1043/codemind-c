80c91a56b6764672dec0191d584749ca6a2ecfe7
Ravikant Sharma <97437176+Ravikant1043@users.noreply.github.com>
2022-10-15 23:23:38 +0530

    #include<stdio.h> int main() {     int i,n,arr[100],c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=n-1;i>=0;i--)     {         if(arr[i]%2==1)         {             printf("%d",i);             break;         }     } }

--- last_odd_number_index_position.c
+++ last_odd_number_index_position.c
@@ -1,16 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int n;
+    int i,n,arr[100],c=0;
     scanf("%d",&n);
-    int a[n],i,c;
-    for (i=0;i<n;i++)
-    scanf("%d",&a[i]);
-    for (i=n-1;i>=0;i--)
+    for(i=0;i<n;i++)
     {
-        if(a[i]%2!=0)
-        break;
+        scanf("%d",&arr[i]);
     }
-    printf("%d",i);
-    return 0;
+    for(i=n-1;i>=0;i--)
+    {
+        if(arr[i]%2==1)
+        {
+            printf("%d",i);
+            break;
+        }
+    }
 }
