09ff2823e5b08aee1796da5ba8437b1a30ef59c4
Ravikant Sharma <97437176+Ravikant1043@users.noreply.github.com>
2022-10-15 23:24:25 +0530

    #include<stdio.h> int main() {     int i,n,arr[100],c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=n-1;i>=0;i--)     {         if(arr[i]%2==0)         {             printf("%d",i);             break;         }     }     return 0; }

--- Last_even_element_index.c
+++ Last_even_element_index.c
@@ -1,16 +1,19 @@
 #include<stdio.h>
 int main()
 {
-    int n;
+    int i,n,arr[100],c=0;
     scanf("%d",&n);
-    int a[n],i;
     for(i=0;i<n;i++)
-    scanf("%d",&a[i]);
+    {
+        scanf("%d",&arr[i]);
+    }
     for(i=n-1;i>=0;i--)
     {
-        if(a[i]%2==0)
-        break;
+        if(arr[i]%2==0)
+        {
+            printf("%d",i);
+            break;
+        }
     }
-    printf("%d",i);
     return 0;
-}
+}  
